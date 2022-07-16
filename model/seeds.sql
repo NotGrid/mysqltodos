507e4cf1b16ebfd4b42276eb72551f4cd6db724e
Andrew <echowhite93@yahoo.com>
2022-07-16 10:26:07 -0700

    push project to github

---
+++ model/seeds.sql
@@ -0,0 +1,9 @@
+USE todos_db;
+
+INSERT INTO todos (todos, isCompleted)
+VALUES ('Run', true),
+        ('Wash dishes', false),
+        ('Fold the laundry', 1),
+        ('Print money', 0),
+        ('Vogue the house down', false),
+        ('Eat dinner');
